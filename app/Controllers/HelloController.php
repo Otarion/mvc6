a1b1d08610a3be14afe1736cd83d190c8c267a8e
Otarion <c.garsault@laposte.net>
2024-02-07 13:42:00 +0100

    Atelier 6 : fin

--- app/Controllers/HelloController.php
+++ app/Controllers/HelloController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use MVC\User;
 
 class HelloController extends Controller
 {
@@ -11,10 +13,13 @@ class HelloController extends Controller
         return $this->response->setContent('Hello world');
     }
 
-    public function hello(string $name): Response
+    public function hello(int $id): Response
     {
-        return $this->view('hello.html', [
-            'name' => $name,
-        ]);
+        try {
+            $user = User::findOrFail($id); // Trouve l'utilisateur par son id ou lance une exception
+            return $this->view('hello.html', ['name' => $user->name]);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new HttpException(404, 'Utilisateur introuvable');
+        }
     }
 }
--- routes.php
+++ routes.php
@@ -5,5 +5,5 @@ use MVC\Route;
 
 return [
     Route::get('/', [HelloController::class, 'index']),
-    Route::get('/hello/{name}', [HelloController::class, 'hello']), // J'ai fait le sournois mon précieux
+    Route::get('/hello/{id}', [HelloController::class, 'hello']), // J'ai fait le sournois mon précieux
 ];
